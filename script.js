19664672eb8230937c132b888e0caf4f972e9b8e
Harichandana-30 <chandanaa296@gmail.com>
2026-05-23 20:09:15 +0530

    Added booking confirmation section

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+const bookingMessage = document.getElementById("booking-message");
+
 document.getElementById("concert-btn")
 .addEventListener("click", function () {
-    alert("Music Concert booked successfully!");
+    bookingMessage.textContent =
+    "🎵 Music Concert booked successfully!";
 });
 
 document.getElementById("tech-btn")
 .addEventListener("click", function () {
-    alert("Tech Conference booked successfully!");
+    bookingMessage.textContent =
+    "💻 Tech Conference booked successfully!";
 });
 
 document.getElementById("food-btn")
 .addEventListener("click", function () {
-    alert("Food Festival booked successfully!");
+    bookingMessage.textContent =
+    "🍔 Food Festival booked successfully!";
 });
