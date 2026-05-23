269a46c8ee4d2924e34d442083d8f1ea2c370d56
Harichandana-30 <chandanaa296@gmail.com>
2026-05-23 20:07:12 +0530

    Added booking button functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("concert-btn")
+.addEventListener("click", function () {
+    alert("Music Concert booked successfully!");
+});
+
+document.getElementById("tech-btn")
+.addEventListener("click", function () {
+    alert("Tech Conference booked successfully!");
+});
+
+document.getElementById("food-btn")
+.addEventListener("click", function () {
+    alert("Food Festival booked successfully!");
+});
